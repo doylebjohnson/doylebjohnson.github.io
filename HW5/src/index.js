5b4c9866307ed85d0cf10da9e8a806911703a024
doylebjohnson <86975472+doylebjohnson@users.noreply.github.com>
2021-12-02 09:28:38 -0800

    Update index.js

--- HW5/src/index.js
+++ HW5/src/index.js
@@ -1,15 +1,13 @@
-window.onload = function() {
-    document.getElementById("button").onclick = async function getDegrees(url) {
-        await fetch(url)
-        .then((response) => response.json())
-        .then((data) =>
-          makeTable(data)
-        );
-    }
-    
-    getDegrees("src/degrees.json");
 
+document.getElementById("button").onclick = async function getDegrees(url) {
+    await fetch(url)
+    .then((response) => response.json())
+    .then((data) =>
+        makeTable(data)
+    );
 }
+
+getDegrees("src/degrees.json");
 
 function makeTable(data) {
     document.write(data.my_degrees[0].major);
