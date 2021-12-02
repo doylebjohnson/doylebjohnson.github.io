09be505c4c6c4eede75be86b0f63e98650c601e1
doylebjohnson <86975472+doylebjohnson@users.noreply.github.com>
2021-12-02 10:12:03 -0800

    Update index.js

--- HW5/src/index.js
+++ HW5/src/index.js
@@ -3,13 +3,14 @@ document.getElementById("button").onclick = function () {
 }
 
 async function getDegrees(url) {
-    // fetch the url
     await fetch(url)
       //get your data here, and check for the response status. If it's not 200, throw an error
       .then((response) => response.json())
       .then((data) =>
-        document.write(
-          `My first degree was a ${data.my_degrees[0].degree.type}`
-        )
+         makeTable(data)
       );
+  }
+
+  function makeTable(data) {
+    document.write(`My first degree was a ${data.my_degrees[0].degree.type}`)
   }
