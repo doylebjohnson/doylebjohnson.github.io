cecbd581d07ce67815b7059d67978693d9d1278d
doylebjohnson <86975472+doylebjohnson@users.noreply.github.com>
2021-12-02 11:15:03 -0800

    updates

--- HW5/src/index.js
+++ HW5/src/index.js
@@ -11,8 +11,27 @@ async function getDegrees(url) {
       );
   }
 
-  function makeTable(data) {
+function makeTable(data) {
     document.write(`My first degree was a ${data.my_degrees[0].degree.type}`)
     var count = Object.keys(data.my_degrees).length;
     console.log(count);
-  }
+
+    let tableData = '<table><thead><tr><th scope="col">University</th><th scope="col">Degree</th><th scope="col">Major</th><th scope="col">Year</th></tr></thead><tbody>';
+
+    for(let i=0; i < count-1; i++) {
+        tableData += '\
+                <tr>\
+                    <td>${data.my_degrees[i].degree.school}</td>\
+                    <td>${data.my_degrees[i].degree.major}</td>\
+                    <td>${data.my_degrees[i].degree.type}</td>\
+                    <td>${data.my_degrees[i].degree.year}</td>\
+                </tr>'
+    }
+
+    tableData +='</tbody></table>';
+
+    document.getElementById("button").remove();
+    document.getElementById("clickTitle").remove();
+    document.getElementById("degreeTable").innerHTML(tableData);
+
+}
