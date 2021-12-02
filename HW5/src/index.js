1ceb04c5fa86c7ac2542ce6d621517ad74e7b02b
doylebjohnson <86975472+doylebjohnson@users.noreply.github.com>
2021-12-02 09:15:11 -0800

    initial commit

---
+++ HW5/src/index.js
@@ -0,0 +1,16 @@
+window.onload = function() {
+    document.getElementById("button").onclick = async function getDegrees(url) {
+        await fetch(url)
+        .then((response) => response.json())
+        .then((data) =>
+          makeTable(data)
+        );
+    }
+    
+    getDegrees("./src/degrees.json");
+
+}
+
+function makeTable(data) {
+    document.write(data.my_degrees[0].major);
+}
