d1d1713c788a8e415991fc85e484882b4a9f7d68
doylebjohnson <86975472+doylebjohnson@users.noreply.github.com>
2021-12-02 11:44:27 -0800

    Update index.js

--- HW5/src/index.js
+++ HW5/src/index.js
@@ -19,10 +19,10 @@ function makeTable(data) {
     let tableData = '<table><thead><tr><th scope="col">University</th><th scope="col">Degree</th><th scope="col">Major</th><th scope="col">Year</th></tr></thead><tbody>';
 
     for(let i=0; i < count-1; i++) {
-        let school = data.mydegrees[i].degree.school;
-        let major = data.mydegrees[i].degree.major;
-        let type = data.mydegrees[i].degree.type;
-        let year = data.mydegrees[i].degree.year;
+        let school = data.my_degrees[i].degree.school;
+        let major = data.my_degrees[i].degree.major;
+        let type = data.my_degrees[i].degree.type;
+        let year = data.my_degrees[i].degree.year;
         tableData += '\
                 <tr>\
                     <td>' + school + '</td>\
