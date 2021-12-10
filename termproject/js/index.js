e8daf5af037349e0f63f476d4420f1f70862c8f9
doylebjohnson <86975472+doylebjohnson@users.noreply.github.com>
2021-12-10 08:33:59 -0800

    updates

--- termproject/js/index.js
+++ termproject/js/index.js
@@ -1,3 +1,18 @@
+window.onload = function() {
+
+    let headshot = document.getElementById("headshot");
+    headshot.addEventListener("mouseover", function() {
+        enlargeImg("this");
+    }, false);
+    headshot.addEventListener("mouseout", function() {
+        standardImg("this");
+    }, false);
+
+    let alcatraz = document.getElementById("alcatraz");
+    alcatraz.addEventListener("mouseover", newImg, false);
+    alcatraz.addEventListener("mouseout", originalImg, false);
+}
+
 function enlargeImg(target) {
     target.style.height = "418px";
     target.style.width = "372px"
